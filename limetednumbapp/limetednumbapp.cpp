7a6e222e1d4fbc897e80b1c3c67a4eb1943918a5
Kostyantyn Malakhov <info@varkon.biz>
2016-11-27 15:48:51 +0200

    Finaly version of the local app

--- limetednumbapp/limetednumbapp.cpp
+++ limetednumbapp/limetednumbapp.cpp
@@ -3,6 +3,8 @@
 
 #include "stdafx.h"
 #include "limetednumbapp.h"
+#include <sstream>
+
 
 
 std::wstring Climetednumbapp::pid;
@@ -30,7 +32,7 @@ LIMETEDNUMBAPP_API int fnlimetednumbapp(void)
 // see limetednumbapp.h for the class definition
 Climetednumbapp::Climetednumbapp(const uint32_t module):_module(module)
 {
-
+   
 }
 Climetednumbapp::~Climetednumbapp()
 {
@@ -41,7 +43,7 @@ int Climetednumbapp::countFiles(const std::wstring& path, const std::wstring& ex
    std::wstring tmp = path +s2ws("*")+ ext;
     WIN32_FIND_DATA FindFileData;
    HANDLE hFind = FindFirstFile(tmp.c_str(), &FindFileData);
-   int count = 0;
+   int count = 1;
    if (hFind != INVALID_HANDLE_VALUE)
    {
       do{
@@ -53,6 +55,32 @@ int Climetednumbapp::countFiles(const std::wstring& path, const std::wstring& ex
    return count;
 
 }
+int Climetednumbapp::readNumberLimit(std::wstring path2file)
+{
+  DWORD dwAttrib = ::GetFileAttributes(path2file.c_str());
+   if (dwAttrib == INVALID_FILE_ATTRIBUTES)
+   {
+      std::ofstream fout(path2file);
+      fout << "[local]"<<std::endl;
+      fout << "COUNT_OF_PROCESS_LOC"<<":"<<COUNT_OF_PROCESS_LOC << std::endl;
+      fout.close();    
+   }
+   std::ifstream fin (path2file);
+   std::string lineread;
+   while(!fin.eof())
+   {
+      std::getline(fin,lineread);
+      if (lineread.find("COUNT_OF_PROCESS_LOC:") !=std::string::npos )
+      {
+         int pos = lineread.find(":")+1;
+         std::istringstream buf(lineread.substr(pos));
+         int num;
+         buf >> num;
+         return num;
+      }
+   }
+   return COUNT_OF_PROCESS_LOC;
+}
 BOOL Climetednumbapp::checkCanWeRun()
 {
    std::wstring path(s2ws(getenv("APPDATA")));
@@ -60,11 +88,17 @@ BOOL Climetednumbapp::checkCanWeRun()
    DWORD dwAttrib = ::GetFileAttributes(path2dir.c_str());
    bool isExsists =  dwAttrib != INVALID_FILE_ATTRIBUTES && (dwAttrib & FILE_ATTRIBUTE_DIRECTORY);
    if (!isExsists && !CreateDirectory(path2dir.c_str(),NULL) )
+   {
       return false;
+   }
+          
    std::string m = std::to_string(HandleToULong(_module));
    std::wstring ext = s2ws(EXT_FILE);
    Climetednumbapp::pid = path2dir+ s2ws(m)+ext;
-   if (Climetednumbapp::countFiles(path2dir,ext) > COUNT_OF_PROCESS_LOC ) return FALSE;
+   if (Climetednumbapp::countFiles(path2dir,ext) > readNumberLimit(path2dir + s2ws(".settings")) ) 
+   {
+      return FALSE;
+   }
    return(_tmpfile = CreateFile(Climetednumbapp::pid.c_str(),GENERIC_READ,0,NULL,CREATE_NEW,FILE_ATTRIBUTE_NORMAL,NULL)) !=  INVALID_HANDLE_VALUE;
 
 }
--- limetednumbapp/limetednumbapp.h
+++ limetednumbapp/limetednumbapp.h
@@ -1,4 +1,5 @@
-
+#ifndef LIMETEDNUMBAPP_H_STOP
+#define LIMETEDNUMBAPP_H_STOP
 #ifdef LIMETEDNUMBAPP_EXPORTS
 #define LIMETEDNUMBAPP_API __declspec(dllexport)
 #else
@@ -8,8 +9,10 @@
 #define COUNT_OF_PROCESS_LOC 4
 #define EXT_FILE             ".pid"
 
+#include <iostream>
 #include <string>
 #include <stdint.h>
+#include <fstream>
 
 // This class is exported from the limetednumbapp.dll
 class LIMETEDNUMBAPP_API Climetednumbapp {
@@ -17,6 +20,7 @@ class LIMETEDNUMBAPP_API Climetednumbapp {
     static HANDLE _tmpfile;
     static std::wstring pid; 
     static int countFiles(const std::wstring& path, const std::wstring& ext);
+    int readNumberLimit(std::wstring path2file);
 public:
    Climetednumbapp(const uint32_t module);
   static void clearModule();
@@ -28,3 +32,4 @@ public:
 extern LIMETEDNUMBAPP_API int nlimetednumbapp;
 
 LIMETEDNUMBAPP_API int fnlimetednumbapp(void);
+#endif
