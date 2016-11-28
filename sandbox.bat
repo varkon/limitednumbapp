48be5330c0cd41ea3f5451c0efcb0388bdd6a9ee
Kostyantyn Malakhov <info@varkon.biz>
2016-11-28 09:56:34 +0200

    Update sandbox.bat

--- sandbox.bat
+++ sandbox.bat
@@ -4,4 +4,4 @@ set ORIGDIR= %CD%
 subst %NDDRIV% /d
 subst %NDDRIV% %ORIGDIR%
 "%ProgramFiles(x86)%\Microsoft Visual Studio 11.0\Common7\IDE\WDExpress.exe"
-rem "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv"
+rem "%ProgramFiles(x86)%\Microsoft Visual Studio 11.0\Common7\IDE\devenv"
